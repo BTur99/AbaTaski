9afad7e7b8cff29b907d4852e48e53aee97fa2be
Gleb <gealtukhov21@gmail.com>
2026-09-16 21:04:36 +0300

    add 16 - 23 whiles + try/except

--- whiles/while23.py
+++ whiles/while23.py
@@ -5,19 +5,16 @@ try:
     b = int(input())
 
     nod = [a, b]
-    print(nod)
     while nod[0] > 0 and nod[1] > 0:
         if nod[0] > nod[1]:
             nod[0] -= nod[1]
-            print(1, nod)
         elif nod[1] > nod[0]:
             nod[1] -= nod[0]
-            print(2, nod)
         else:
-            nod[0]
+            nod[0] -= nod[1]
 
 
-    print(nod[0])
+    print(nod[1])
 
 
 except:
